fb0fffa4eb8f9ba58beb54467b55493e04b6a3e3
MarkAWard <markmaw@gmail.com>
2013-12-23 23:29:41 -0500

    echo back message too

--- run.py
+++ run.py
@@ -8,19 +8,18 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
+    
+    message = str(request.values.get('body', None))
+
     page = urllib2.urlopen("http://www.goremountain.com/mountain/snow-report")
     soup = BeautifulSoup(page)
     trails  = str(soup.find("div", "alpineTrailsLeft").get_text())
     lifts   = str(soup.find("div", "alpineLiftsLeft").get_text())
     resp = twiml.Response()
-    resp.message("Hello! Ski report for Gore Mountain: \n" + trails + lifts)
+    resp.message(message +" Ski report for Gore Mountain: \n" + trails + lifts)
     return str(resp)
 
 
-@app.route("/other")
-def next():
-
-    return "go away World!"
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))
