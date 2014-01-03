aae935f36ae98c19def25dc7f06521c12be11e40
MarkAWard <markmaw@gmail.com>
2014-01-02 23:57:00 -0500

    Gore_Conditions function

--- run.py
+++ run.py
@@ -7,35 +7,49 @@ from time import strftime, localtime
 
 app = Flask(__name__)
 
+def Gore_Conditions():
+    
+    page = urllib2.urlopen("http://www.goremountain.com/mountain/snow-report")
+    soup = BeautifulSoup(page)
+    
+    trails   = str(soup.find("div", "alpineTrailsLeft").get_text())
+    lifts    = str(soup.find("div", "alpineLiftsLeft").get_text())
+    depth    = soup.find("div", "alpineConditionsRightRow")
+    surface1 = depth.find_next().find_next()
+    surface2 = surface1.find_next().find_next()
+    skiBowl  = surface2.find_next().find_next().find_next()
+    newSnow  = skiBowl.find_next().find_next()
+    makeSnow = newSnow.find_next().find_next()
+    depth    = str(depth.get_text()) 
+    surface1 = str(surface1.get_text()) 
+    surface2 = str(surface2.get_text()) 
+    skiBowl  = str(skiBowl.get_text())
+    newSnow  = str(newSnow.get_text())
+    makeSnow = str(makeSnow.get_text())
+    
+    response = "Ski report for Gore Mountain: \n" + trails + lifts  +'\n'+ depth +'\n'+ surface1 +'\n'+ surface2 +'\n'+ skiBowl +'\n'+ newSnow +'\n'+ makeSnow
+
+    return response
+
+
 @app.route("/", methods=['GET', 'POST'])
 def ski_report():
     
-    body = request.values.get("Body", None)
-    body = "Gore UpDate WeaTHer"
-    text = body.lower().split(' ')
-    resp = twiml.Response()
+    try:
+        body = request.values.get("Body", None)
+        text = body.lower().split(' ')
+    except:
+        body = "NONE "
+        text =  body.lower().split(' ')
 
     if text[0] == "gore":
-        page = urllib2.urlopen("http://www.goremountain.com/mountain/snow-report")
-        soup = BeautifulSoup(page)
-
-        trails  = str(soup.find("div", "alpineTrailsLeft").get_text())
-        lifts   = str(soup.find("div", "alpineLiftsLeft").get_text())
-        depth   = soup.find("div", "alpineConditionsRightRow")
-        surface1= depth.find_next().find_next()
-        surface2= surface1.find_next().find_next()
-        skiBowl = surface2.find_next().find_next().find_next()
-        newSnow = skiBowl.find_next().find_next()
-        makeSnow= newSnow.find_next().find_next()
-
-        
-        resp_body = "Ski report for Gore Mountain: \n" + trails + lifts  +'\n'+ str(depth.get_text()) +'\n'+ str(surface1.get_text()) +'\n'+ str(surface2.get_text()) +'\n'+ str(skiBowl.get_text()) +'\n'+ str(newSnow.get_text()) +'\n'+ str(makeSnow.get_text())
-
-
+        resp_body = GoreConditions()
     else:
         resp_body = "Sorry we do not currently support '" + text[0] + "'. Trying sending 'Gore' for realtime mountain updates."
 
     resp_body += "\n\n" + strftime("%a, %d %b %Y %X", localtime())
+
+    resp = twiml.Response()
     resp.message(resp_body)
     return str(resp)
 
